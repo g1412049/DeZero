d0d7194ec08d1e4ad2a0ed2489158a41d9991f26
Koji Shibata <kshibata.0519@gmail.com>
2020-10-23 21:32:38 +0900

    DeZero: Step 14まで実装．各クラスや関数にコメントを追加．

--- trunk/main.py
+++ trunk/main.py
@@ -3,29 +3,39 @@ import numpy as np
 import unittest
 
 # User module
-# Variable class
+# --Variable class--
 class Variable:
+    # Variable class インスタンス
+    # クラス初期化を実施
     def __init__(self, data):
+        # 入力データがNoneでかつnp.ndarrayでない場合はエラー生成
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError('{} is not supported'.format(type(data)))
-
+        # 入力データをメンバ変数に代入
         self.data = data
+        # 勾配情報と生成元関数を設定するメンバ変数を作成
         self.grad = None
         self.creator = None
-        
+
+    # 勾配情報を消去するメゾット
     def cleargrad(self):
         self.grad = None
 
+    # 関数と出力変数を関連付けるメゾット
     def set_creator(self, func):
         self.creator = func
 
+    # 自動微分を計算するメゾット
     def backward(self):
+        # 勾配を記述するメンバ変数self.gradがNoneの場合
+        # 既存の配列と同じサイズですべての要素の値が 1 の配列を生成
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
         funcs = [self.creator]
         while funcs:
+            # 指定した位置の要素を削除して値を取得
             f = funcs.pop()
 
             gys = [output.grad for output in f.outputs]
@@ -42,6 +52,9 @@ class Variable:
             if x.creator is not None:
                 funcs.append(x.creator)
 
+
+# --Function class--
+# 各関数の基本構造を記述した抽象クラス
 class Function:
     def __call__(self, *inputs):
         xs = [x.data for x in inputs]
@@ -63,6 +76,8 @@ class Function:
     def backward(self, gy):
         raise NotImplementedError()
 
+# --Add class--
+# 各変数を足し合わせる関数のクラス
 class Add(Function):
     def forward(self, x0, x1):
         y = x0 + x1
@@ -70,6 +85,8 @@ class Add(Function):
     def backward(self, gy):
         return gy, gy
 
+# --Square class--
+# 変数に対して2乗を計算する関数のクラス
 class Square(Function):
     def forward(self, x):
         return x ** 2
@@ -79,6 +96,9 @@ class Square(Function):
         gx = 2 * x * gy
         return gx
 
+
+# --Exp class--
+# 変数に対してexpを計算する関数のクラス
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
@@ -102,15 +122,19 @@ class SquareTest(unittest.TestCase):
         expected = np.array(6.)
         self.assertEqual(x.grad, expected)
 
+# 各変数を足し合わせる関数
 def add(x0, x1):
     return Add()(x0, x1)
 
+# 変数に対して2乗を計算する関数
 def square(x):
     return Square()(x)
 
+# 変数に対してexpを計算する関数
 def exp(x):
     return Exp()(x)
 
+# 入力引数がスカラであった場合は行列形式に変換する関数
 def as_array(x):
     if np.isscalar(x):
         return np.array(x)
@@ -131,7 +155,6 @@ def f(x):
 
 def main():
     x = Variable(np.array(3.))
-    y = Variable(np.array(3.))
     z = add(add(x, x), x)
     z.backward()
     print(z.data)
